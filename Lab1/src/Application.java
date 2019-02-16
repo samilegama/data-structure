682573c979260748f592cca942da1e056f96bbf2
Samuel Ebba <43682196+samilegama@users.noreply.github.com>
2019-02-16 11:26:51 -0500

    Add files via upload

---
+++ Lab1/src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+	private static int x = 15;
+	private static int y = 10;
+
+    public static void main(String[] args) {
+
+        System.out.println("The product of x and y is: " +(x*y));
+        System.out.println("The sum of x and y is: " +(x+y));
+        System.out.println("The difference of x and y is: " +(x-y));
+        System.out.println("The division of x and y is: " +(x/y));
+        
+    }
+}
+
